e18731d900cd92e153c4ddb10b3238d4a8cba9fe
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2022-06-02 16:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('attr.pkl', 'rb'))
 
 app = Flask(__name__)
 
